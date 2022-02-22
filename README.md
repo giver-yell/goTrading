690b546a0a32eebffc08489b0047fe53148fd8f1
Ryohei Fujihara <giver.yell@gmail.com>
2022-02-22 09:26:15 +0900

    102.DBスキーマを作成 #7

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # goTrading
 
 ## bitflyer
+
 ### 公式リファレンス
+
 `https://lightning.bitflyer.com/docs?lang=ja`
 
 ## JSON-to-Go
+
 `https://mholt.github.io/json-to-go/`
+
+## ダウンロードが必要な third party
+
+`go get github.com/mattn/go-sqlite3`
--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/giver-yell/goTrading/bitflyer"
+	"github.com/giver-yell/goTrading/app/models"
 	"github.com/giver-yell/goTrading/config"
 	"github.com/giver-yell/goTrading/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	// apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
 	// fmt.Println(apiClient.GetBalance())
 
 	// ticker, _ := apiClient.GetTicker("BTC_USD")
@@ -30,16 +30,18 @@ func main() {
 	// 	fmt.Println(ticker.TruncateDateTime(time.Hour))
 	// }
 
-	order := &bitflyer.Order{
-		ProductCode: config.Config.ProductCode,
-		// 成り行き
-		ChildOrderType:  "MARKET",
-		Side:            "BUY",
-		Size:            0.01,
-		MinuteToExpires: 1,
-		TimeInForce:     "GTC",
-	}
-	res, _ := apiClient.SendOrder(order)
-	fmt.Println(res.ChildOrderAcceptanceID)
+	// order := &bitflyer.Order{
+	// 	ProductCode: config.Config.ProductCode,
+	// 	// 成り行き
+	// 	ChildOrderType:  "MARKET",
+	// 	Side:            "BUY",
+	// 	Size:            0.01,
+	// 	MinuteToExpires: 1,
+	// 	TimeInForce:     "GTC",
+	// }
+	// res, _ := apiClient.SendOrder(order)
+	// fmt.Println(res.ChildOrderAcceptanceID)
+
+	fmt.Println(models.DbConnection)
 
 }
